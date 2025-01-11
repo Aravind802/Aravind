a37616a49f5b1844c45331be078afaa2881501c7
Aravind <aravindyadavv.261124@gmail.com>
2025-01-11 21:04:37 +0530

    Add files via upload

---
+++ day2 (1).py
@@ -0,0 +1,11 @@
+my_list = [5, 10, 15, 20, 25]
+print(my_list[1])
+print(my_list[2:4])
+
+my_tuple = (50, 100, 150, 200, 250)
+print(my_tuple[3])
+print(my_tuple[:2])
+
+my_dict = {"apple": 1, "banana": 2, "cherry": 3, "date": 4, "elderberry": 5}
+print(my_dict["banana"])
+print(list(my_dict.values())[0:3])
