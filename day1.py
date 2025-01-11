e8007f9f23d4a61a2b631c58b9a60e957a271312
Aravind <aravindyadavv.261124@gmail.com>
2025-01-11 15:20:41 +0000

    any name about project

---
+++ day1.py
@@ -0,0 +1,9 @@
+integer_var = 10
+float_var = 10.5
+string_var = "Hello"
+boolean_var = True
+
+print(integer_var, type(integer_var))
+print(float_var, type(float_var))
+print(string_var, type(string_var))
+print(boolean_var, type(boolean_var))
