72d5241b50977cba282ee97ee87eca6ff376e8d0
Aravind <aravindyadavv.261124@gmail.com>
2026-01-05 10:21:16 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class PromptRequest(BaseModel):
-    prompt: str = Field(
-        ...,
-        min_length=1,
-        description="User prompt to send to all LLMs"
-    )
